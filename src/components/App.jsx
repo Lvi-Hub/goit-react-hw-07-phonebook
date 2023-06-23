34d668d07eada2c869877b7ebf82ca0e896827d7
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-23 23:22:28 +0200

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from '../redux/operations';
+import { getTasks } from '../redux/selectors';
+
 export const App = () => {
-  return <div className="app">React homework template</div>;
+  const dispatch = useDispatch();
+  const { items, isLoading, error, petro } = useSelector(getTasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  console.log(items);
+  return (
+    <div>
+      {isLoading && <b>Loading tasks...</b>}
+      {error && <b>{error}</b>}
+      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
+    </div>
+  );
 };
